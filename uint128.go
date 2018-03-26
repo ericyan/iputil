20356d23b68aa4e8271d49decaab8fc98b376e45
Eric Yan <github@ericyan.me>
2018-03-26 23:13:22 +0800

    Implement decimal string for uint128

--- uint128.go
+++ uint128.go
@@ -2,6 +2,7 @@ package iputil
 
 import (
 	"encoding/binary"
+	"math/big"
 )
 
 // uint128 is a big-endian unsigned 128-bit integer.
@@ -21,4 +22,28 @@ func (x uint128) bytes() [16]byte {
 	binary.BigEndian.PutUint64(buf[:8], x[0])
 	binary.BigEndian.PutUint64(buf[8:], x[1])
 	return buf
+}
+
+// setDecimalString sets x to the value of s, interpreted in base 10,
+// and returns x as well as a boolean indicating success.
+func (x uint128) setDecimalString(s string) (uint128, bool) {
+	i, ok := new(big.Int).SetString(s, 10)
+	if !ok {
+		return x, false
+	}
+
+	var b [16]byte
+	if buf := i.Bytes(); len(buf) > 16 {
+		return x, false
+	} else {
+		copy(b[16-len(buf):], buf)
+	}
+
+	return x.setBytes(b), true
+}
+
+// decimalString converts x to a string of decimal digits.
+func (x uint128) decimalString() string {
+	b := x.bytes()
+	return new(big.Int).SetBytes(b[:]).String()
 }
--- uint128_test.go
+++ uint128_test.go
@@ -13,3 +13,28 @@ func TestBytes(t *testing.T) {
 		t.Errorf("unexpected byte slice: got %v, want %v", xbuf, buf)
 	}
 }
+
+func TestDecimal(t *testing.T) {
+	cases := []struct {
+		in  string
+		ok  bool
+		out string
+	}{
+		{"", false, "0"},
+		{"0", true, "0"},
+		{"0123456", true, "123456"},
+		{"4294967295", true, "4294967295"},
+		{"340282366920938463463374607431768211455", true, "340282366920938463463374607431768211455"},
+		{"340282366920938463463374607431768211456", false, "0"},
+	}
+
+	for _, c := range cases {
+		x, ok := new(uint128).setDecimalString(c.in)
+		if ok != c.ok {
+			t.Errorf("unexpected result: got %t, want %t", ok, c.ok)
+		}
+		if out := x.decimalString(); out != c.out {
+			t.Errorf("unexpected result: got %s, want %s", out, c.out)
+		}
+	}
+}
