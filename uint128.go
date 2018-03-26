ad2f71053d302c568df1ca3b56db301b429c24cc
Eric Yan <github@ericyan.me>
2018-03-26 22:51:58 +0800

    Use [16]byte instead of []byte for uint128

--- uint128.go
+++ uint128.go
@@ -9,15 +9,15 @@ type uint128 [2]uint64
 
 // setBytes interprets buf as the bytes of a big-endian uint128, sets x
 // to that value, and returns x.
-func (x uint128) setBytes(b []byte) uint128 {
+func (x uint128) setBytes(b [16]byte) uint128 {
 	x[0] = binary.BigEndian.Uint64(b[:8])
 	x[1] = binary.BigEndian.Uint64(b[8:])
 	return x
 }
 
 // bytes returns x as a big-endian byte slice.
-func (x uint128) bytes() []byte {
-	buf := make([]byte, 16)
+func (x uint128) bytes() [16]byte {
+	var buf [16]byte
 	binary.BigEndian.PutUint64(buf[:8], x[0])
 	binary.BigEndian.PutUint64(buf[8:], x[1])
 	return buf
--- uint128_test.go
+++ uint128_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestBytes(t *testing.T) {
-	buf := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	buf := [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	x := new(uint128).setBytes(buf)
 
-	if xbuf := x.bytes(); !bytes.Equal(xbuf, buf) {
+	if xbuf := x.bytes(); !bytes.Equal(xbuf[:], buf[:]) {
 		t.Errorf("unexpected byte slice: got %v, want %v", xbuf, buf)
 	}
 }
