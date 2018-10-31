35969e5699caf4ef94533cda5b20ba6803c71f51
Eric Yan <github@ericyan.me>
2018-10-31 22:52:01 +0800

    Implement Range to CIDR conversion

--- range.go
+++ range.go
@@ -61,6 +61,43 @@ func (r *Range) Contains(ip net.IP) bool {
 	return true
 }
 
+// CIDR returns CIDR notation(s) for the range.
+func (r *Range) CIDR() []*net.IPNet {
+	results := make([]*net.IPNet, 0)
+
+	var maxPrefix int
+	switch r.af {
+	case IPv4:
+		maxPrefix = IPv4BitLen
+	case IPv6:
+		maxPrefix = IPv6BitLen
+	}
+
+	cur := r.first
+	for cur.IsLessThan(r.last) || cur.IsEqualTo(r.last) {
+		// Number of zeros in netmask to represent current IP.
+		zeros := cur.TrailingZeros()
+
+		// Number of remaining IPs.
+		n := r.last.Sub(cur).Add(uint128.Uint128{0, 1})
+
+		// log2n is the max k that 2**k < n. This ensures cidrLen < n.
+		if log2n := 128 - n.LeadingZeros() - 1; zeros > log2n {
+			zeros = log2n
+		}
+
+		results = append(results, &net.IPNet{
+			IP:   cur.Bytes()[16-maxPrefix/8:],
+			Mask: net.CIDRMask(maxPrefix-zeros, maxPrefix),
+		})
+
+		cidrLen, _ := uint128.Pow2(uint(zeros))
+		cur = cur.Add(cidrLen)
+	}
+
+	return results
+}
+
 // Network returns the network name, "ip+net".
 func (r *Range) Network() string {
 	return "ip+net"
--- range_test.go
+++ range_test.go
@@ -5,24 +5,29 @@ import (
 	"testing"
 )
 
+var (
+	ipv4Range, _ = NewRange(ParseIPv4("192.168.0.100"), ParseIPv4("192.168.0.199"))
+	ipv6Range, _ = NewRange(ParseIPv6("2001:db8::1234:0"), ParseIPv6("2001:db8::5678:0"))
+)
+
 func TestRange(t *testing.T) {
-	ipv4Range, _ := NewRange(ParseIPv4("192.168.0.100"), ParseIPv4("192.168.0.199"))
 	if str := ipv4Range.String(); str != "192.168.0.100 - 192.168.0.199" {
 		t.Errorf("unexpected string for IPv4 range: %s", str)
 	}
 
-	ipv6Range, _ := NewRange(ParseIPv6("2001:0db8::1234:1"), ParseIPv6("2001:0db8::5678:1"))
-	if str := ipv6Range.String(); str != "2001:db8::1234:1 - 2001:db8::5678:1" {
+	if str := ipv6Range.String(); str != "2001:db8::1234:0 - 2001:db8::5678:0" {
 		t.Errorf("unexpected string for IPv6 range: %s", str)
 	}
 
-	if _, err := NewRange(ParseIPv4("192.168.0.100"), ParseIPv6("2001:0db8::5678:1")); err == nil {
+	if _, err := NewRange(ParseIPv4("192.168.0.100"), ParseIPv6("2001:db8::5678:0")); err == nil {
 		t.Error("error expected for invalid range")
 	}
-	if _, err := NewRange(ParseIPv4("192.168.0.100"), ParseIPv6("192.168.0.99")); err == nil {
+	if _, err := NewRange(ParseIPv4("192.168.0.100"), ParseIPv4("192.168.0.99")); err == nil {
 		t.Error("error expected for invalid range")
 	}
+}
 
+func TestRangeContains(t *testing.T) {
 	cases := []struct {
 		Range  *Range
 		IP     net.IP
@@ -34,15 +39,54 @@ func TestRange(t *testing.T) {
 		{ipv4Range, ParseIPv4("192.168.0.199"), true},
 		{ipv4Range, ParseIPv4("192.168.0.200"), false},
 		{ipv6Range, ParseIPv6("2001:0db8::1"), false},
-		{ipv6Range, ParseIPv6("2001:0db8::1234:1"), true},
+		{ipv6Range, ParseIPv6("2001:0db8::1234:0"), true},
 		{ipv6Range, ParseIPv6("2001:0db8::3456:1"), true},
-		{ipv6Range, ParseIPv6("2001:0db8::5678:1"), true},
+		{ipv6Range, ParseIPv6("2001:0db8::5678:0"), true},
 		{ipv6Range, ParseIPv6("2001:0db8::abcd:1"), false},
 	}
 
 	for _, c := range cases {
 		if result := c.Range.Contains(c.IP); result != c.Result {
 			t.Errorf("unexpected result for %s: got %t, want %t", c.IP, result, c.Result)
+		}
+	}
+}
+
+func TestRangeCIDR(t *testing.T) {
+	ipv4CIDRs := []string{
+		"192.168.0.100/30",
+		"192.168.0.104/29",
+		"192.168.0.112/28",
+		"192.168.0.128/26",
+		"192.168.0.192/29",
+	}
+	for i, cidr := range ipv4Range.CIDR() {
+		if cidr.String() != ipv4CIDRs[i] {
+			t.Errorf("unexpected CDIR: got %s, want %s", cidr, ipv4CIDRs[i])
+		}
+	}
+
+	ipv6CIDRs := []string{
+		"2001:db8::1234:0/110",
+		"2001:db8::1238:0/109",
+		"2001:db8::1240:0/106",
+		"2001:db8::1280:0/105",
+		"2001:db8::1300:0/104",
+		"2001:db8::1400:0/102",
+		"2001:db8::1800:0/101",
+		"2001:db8::2000:0/99",
+		"2001:db8::4000:0/100",
+		"2001:db8::5000:0/102",
+		"2001:db8::5400:0/103",
+		"2001:db8::5600:0/106",
+		"2001:db8::5640:0/107",
+		"2001:db8::5660:0/108",
+		"2001:db8::5670:0/109",
+		"2001:db8::5678:0/128",
+	}
+	for i, cidr := range ipv6Range.CIDR() {
+		if cidr.String() != ipv6CIDRs[i] {
+			t.Errorf("unexpected CDIR: got %s, want %s", cidr, ipv6CIDRs[i])
 		}
 	}
 }
