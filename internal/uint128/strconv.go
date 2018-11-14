953caa3c6991e9f819007cc156d6f0f16f5b7daf
Eric Yan <github@ericyan.me>
2018-11-14 21:33:04 +0800

    Implment Itoa using uint128 arithmetic operations
    This commit drops the dependency of math/big.

--- internal/uint128/strconv.go
+++ internal/uint128/strconv.go
@@ -1,13 +1,27 @@
 package uint128
 
-import "math/big"
+const digits = "0123456789"
 
 // cutoff is the smallest n that n*10 will overlow an Int.
 var cutoff = Int{1844674407370955161, 11068046444225730970}
 
 // Itoa returns the string representation of x in base 10.
 func Itoa(x Int) string {
-	return new(big.Int).SetBytes(x.Bytes()).String()
+	s := make([]byte, 0)
+	for x.IsGreaterThan(Int{0, 10}) {
+		quo, mod := div(x, Int{0, 10})
+
+		s = append(s, digits[int(mod.Lo)])
+		x = quo
+	}
+	s = append(s, digits[int(x.Lo)])
+
+	// reverse the string
+	for l, r := 0, len(s)-1; l < r; l, r = l+1, r-1 {
+		s[l], s[r] = s[r], s[l]
+	}
+
+	return string(s)
 }
 
 // Atoi creates a new Int from s, interpreted in base 10.
